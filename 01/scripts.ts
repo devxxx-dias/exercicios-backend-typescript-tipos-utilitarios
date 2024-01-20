76e84af3c9292a868d008d64a27bf3d8bcbaf10f
Maurício Dias <devxxx.mauricio@gmail.com>
2024-01-20 11:07:40 -0300

    Exercicio Tipos de Dados Utilitarios Feito

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -1,11 +1,24 @@
 type Usuario = {
-    nome: string,
-    email: string,
-    cpf: string,
-    rg: string
-    dataNacimento?: string
-}
+  nome: string;
+  email: string;
+  cpf: string;
+  rg: string;
+  dataNacimento?: string;
+};
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
-    return info;
-}
+  return info;
+};
+
+const cadastroSemRg = (
+  info: Required<Omit<Usuario, "rg">>
+): Omit<Usuario, "rg"> => {
+  return info;
+};
+
+const pessoa1 = cadastroSemRg({
+  nome: "Jose",
+  email: "jose@email.com",
+  cpf: "33333333314",
+  dataNacimento: "07/02/2004",
+});
--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,21 +1,60 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
-}
+  nome: string;
+  descricao: string;
+  valor: number;
+};
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
-}
+  numero: number;
+  validade: string;
+  nome: string;
+  cvv: number;
+};
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
-}
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  tipoTransacao: "credito" | "Debito";
+  cartao: Cartao;
+};
+
+type Endereco = {
+  rua: string;
+  bairro: string;
+  cidade: string;
+  cep: string;
+  estado: string;
+  tipoTransacao: Lowercase<"Debito" | "Credito">;
+};
+
+type CarrinhoEnderecoTransacaoMinuscula = Omit<Carrinho, "tipoTransacao"> &
+  Endereco;
+
+const carrinho = (info: CarrinhoEnderecoTransacaoMinuscula) => {
+  return info;
+};
+
+carrinho({
+  tipoTransacao: "debito",
+  bairro: "centro",
+  cartao: {
+    numero: 123456,
+    validade: "string",
+    nome: "string",
+    cvv: 120,
+  },
+  cep: "1234564564",
+  cidade: "Amazonas",
+  desconto: 10,
+  estado: "BA",
+  frete: 12,
+  item: {
+    nome: "string",
+    descricao: "string",
+    valor: 122,
+  },
+  qtd: 2,
+  rua: "Rua 3",
+});
