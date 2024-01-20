02069bcd77a740f11999ae3b20fe3fb3dce6c1f2
Maurício Dias <devxxx.mauricio@gmail.com>
2024-01-20 11:42:38 -0300

    Adicionando a correcao apropriada readonly

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,10 +6,26 @@ type Conn = {
   dbname: string;
 };
 
-const conexao = (dados: Readonly<Conn>) => {
+type Connection = {
+  driver: string;
+  url: string;
+};
+
+//Para usar o comando Readonly - voce precisa criar um tipo especifico pra isso para ser usado como retorno, pois do contrario ainda sim e possivel alterar.
+const conexao = (dados: Conn): Readonly<Connection> => {
   const { username, password, host, port, dbname } = dados;
   return {
     driver: "postgres",
     url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
   };
 };
+
+let conn = conexao({
+  username: "string",
+  password: "string",
+  host: "string",
+  port: "string",
+  dbname: "string",
+});
+
+// conn.driver = "amor" ;
